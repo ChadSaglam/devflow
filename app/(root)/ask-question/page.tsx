fec3b5aac6da3ee7b87b1910b39f586724ba210e
Chad Saglam <saglam.chad@chadev.ch>
2024-11-22 16:38:30 +0100

    LeftSidebar

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 
-import QuestionForm from "@/components/forms/QuestionForm";
-
 const AskAQuestion = () => {
-  return (
-    <>
-      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
-
-      <div className="mt-9">
-        <QuestionForm />
-      </div>
-    </>
-  );
+  return <div>AskAQuestion</div>;
 };
 
 export default AskAQuestion;
